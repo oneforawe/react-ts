6e161648289ab30d7685a6119a46595be3659d15
Andrew Forrester <awforrester@gmail.com>
2024-01-23 16:40:37 -0800

    Add another link reference to craco config.

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,7 @@
 require('json5/lib/register'); // craco+react-app-alias needs this json5 support
 const { CracoAliasPlugin } = require('react-app-alias');
 
-
+// See https://github.com/oklas/react-app-alias/issues/92
 // require.extensions deprecated, but no alternatives?
 // See https://github.com/nodejs/node/issues/32483
 require.extensions['.json'] = require.extensions['.json5'];
