82f70580870d110e2439382dea0429e9e745cddf
Andrew Forrester <awforrester@gmail.com>
2024-01-30 18:40:28 -0800

    Save more experimental work on moving tsconfig into config folder. Seem to be closer, but new craco.config.js settings are not working yet when running 'npm start'.

--- .config/craco.config.js
+++ .config/craco.config.js
@@ -1,11 +1,29 @@
 require('json5/lib/register'); // craco+react-app-alias needs this json5 support
-const { CracoAliasPlugin } = require('react-app-alias');
+
+const {
+  CracoAliasPlugin, configPaths, aliasWebpack, // aliasJest,
+} = require('react-app-alias');
+
 
 // See https://github.com/oklas/react-app-alias/issues/92
 // require.extensions deprecated, but no alternatives?
 // See https://github.com/nodejs/node/issues/32483
 require.extensions['.json'] = require.extensions['.json5'];
 
+
+const aliasMap = configPaths('./tsconfig.paths.json');
+
+const cracoAliasOptions = {
+  // default is empty for most cases (but not given this repo's .config folder)
+  tsconfig: './tsconfig.json',
+  baseUrl:  '../src',
+  alias:    aliasMap,
+};
+
+
+module.exports = aliasWebpack(aliasMap);
+module.exports = aliasWebpack(cracoAliasOptions);
+// module.exports.jest = aliasJest(cracoAliasOptions);
 
 module.exports = {
   eslint: {
@@ -14,7 +32,7 @@ module.exports = {
   plugins: [
     {
       plugin:  CracoAliasPlugin,
-      options: {}, // default is empty for most cases
+      options: cracoAliasOptions,
     },
   ],
 };
