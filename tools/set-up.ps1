336b21a59a730eca745be89d969dc5d8fbb0e509
Andrew Forrester <andrew.forrester@pacden.com>
2024-02-18 20:33:17 -0800

    Re-order steps in set-up scripts to get rid of step in README, and integrate that removed step as a note in the scripts. Add print-out statements for script steps. Modify comments for clarity and consistency.

--- tools/set-up.ps1
+++ tools/set-up.ps1
@@ -5,39 +5,23 @@
 #            .\tools\set-up.ps1
 
 
-# 1. Install the packages.
-#    `npm install`
-npm install
-# As of 2024-01-24 there are about 14 deprecation warnings and 9
-# vulnerabilities (3 moderate, 6 high) expected and the `package-lock.json`
-# file is not altered by the installation. The warnings and vulnerabilities are
-# [not concerning](https://github.com/facebook/create-react-app/issues/11174).
-# After installation, you can check to see if the package-lock file was
-# altered, and decide whether this is a concern; note to authors -- in that case
-# maybe this repo should be updated.
-
-
-# 2. Re-initialize the repo.
+# 1. Reset and initialize the repo for git.
+Write-Output "Resetting and initializing the repo for git."
 #    1. Delete the git folder.
-#       For example, execute `rm -rf .git` (a Bash command) or
-#       `Remove-Item -Recurse -Force .git` (a PowerShell command).
+#       For Bash:       `rm -rf .git`
+#       For PowerShell: `Remove-Item -Recurse -Force .git`
 Remove-Item -Recurse -Force .git
 #    2. Initialize as a new git repo.
 #       `git init`
 git init
 #    3. Enable the root config file.
-#        `git config --local include.path ../.gitconfig`
+#       (Modify the local git config to use the root gitconfig file.)
+#       `git config --local include.path ../.gitconfig`
 git config --local include.path ../.gitconfig
 
 
-# 3. Create a secrets file.
-#    `cp src/config/secrets_template.ts src/config/secrets.ts`
-Copy-Item src/config/secrets_template.ts src/config/secrets.ts
-#    This new secrets file will also be ignored, since it's included in the ignore
-#    file.
-
-
-# 4. Fix any incorrect file line-endings.
+# 2. Fix any incorrect file line-endings.
+Write-Output "Fixing any incorrect file line-endings."
 #    1. Use the temporary git config file with a copy of the git attributes file
 #        at the root of the repo to allow the following steps to work properly.
 #        `rm .gitconfig`
@@ -62,7 +46,7 @@ git add .
 #    4. If there are warnings, you can automatically correct the files by
 #        deleting all of the (non-ignored) files in the working tree and then
 #        check them out from the index:
-#        For Bash: `git ls-files -z | xargs -0 rm -f`
+#        For Bash:       `git ls-files -z | xargs -0 rm -f`
 #        For PowerShell: `git ls-files | ForEach-Object {Remove-Item $_}`
 git ls-files | ForEach-Object {Remove-Item $_}
 #        `git checkout .`  (for both shells)
@@ -79,3 +63,36 @@ git rm --cached -r .
 Remove-Item .gitattributes
 Remove-Item .gitconfig
 Copy-Item .config/.gitconfig_sample .gitconfig
+
+# Note on fixing "by hand" any incorrect file line-endings a file.
+#   1. Open the repo root folder `react-ts` in VSCode by executing
+#      `code .`
+#   2. Select a file to view in the (primary)
+#      [side bar](https://code.visualstudio.com/docs/getstarted/userinterface) /
+#      Explorer.
+#   3. Note that for any viewed file, you should see a line-ending indicator,
+#      showing either "LF" or "CRLF", in the lower right corner of the window in
+#      the [status bar](https://code.visualstudio.com/docs/getstarted/userinterface).
+#      If you ever need to change the line-endings type of a file, you can click
+#      on that indicator to initiate that change.
+
+
+# 3. Create a secrets file.
+Write-Output "Creating a secrets file."
+#    `cp src/config/secrets_template.ts src/config/secrets.ts`
+Copy-Item src/config/secrets_template.ts src/config/secrets.ts
+#    This new secrets file will also be ignored, since it's included in the
+#    ignore file.
+
+
+# 4. Install the npm packages.
+Write-Output "Installing the npm packages."
+#    `npm install`
+npm install
+# As of 2024-01-24 there are about 14 deprecation warnings and 9
+# vulnerabilities (3 moderate, 6 high) expected and the `package-lock.json`
+# file is not altered by the installation. The warnings and vulnerabilities are
+# [not concerning](https://github.com/facebook/create-react-app/issues/11174).
+# After installation, you can check to see if the package-lock file was
+# altered, and decide whether this is a concern; note to authors -- in that case
+# maybe this repo should be updated.
